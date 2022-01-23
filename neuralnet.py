5803078b670922993f7030672bd6af461a88b2da
Gustavo Soares <gustavosoares112@gmail.com>
2022-01-23 16:06:20 -0300

    train function

--- neuralnet.py
+++ neuralnet.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 
 class NeuralNetwork:
@@ -16,6 +17,44 @@ class NeuralNetwork:
 
         self.activation_function = lambda x: 1 / (1 + np.exp(-x))  # sigmoid
         self.loss_function = lambda y, Y: np.mean((y - Y) ** 2)  # mean-squared error
+
+  
+    def train(self, features, targets, epochs, val_features, val_targets, batch_size=100):
+
+        delta_weights_i_h = np.zeros(self.weights_input_to_hidden.shape)
+        delta_weights_h_o = np.zeros(self.weights_hidden_to_output.shape)
+
+        self.losses = {'train':[], 'validation':[]}
+
+        for epoch in range(epochs):
+            batch = np.random.choice(range(features.shape[0]), size=batch_size)
+            batch_features, batch_targets = features[batch], targets[batch]
+            
+            delta_weights_i_h *= 0
+            delta_weights_h_o *= 0
+
+            for X, y in zip(batch_features, batch_targets):
+                hidden_outputs, final_outputs = self.forward(X)
+
+                delta_weights_i_h, delta_weights_h_o = self.backpropagation(X, y, final_outputs, hidden_outputs,
+                                                                            delta_weights_i_h, delta_weights_h_o)
+            
+            self.update_weights(delta_weights_i_h, delta_weights_h_o, batch_size)
+
+            output_train = self.predict(features).T
+            output_val = self.predict(val_features).T
+
+            train_loss = self.loss_function(output_train, targets)
+            val_loss = self.loss_function(output_val, val_targets)
+
+            sys.stdout.write("\rEpoch {}/{}".format(epoch + 1, epochs) \
+                            + "\tTraining Loss:" + str(train_loss)[:5]
+                            + "\tValidation Loss:" + str(val_loss)[:5])
+
+            sys.stdout.flush()
+
+            self.losses['train'].append(train_loss)
+            self.losses['validation'].append(val_loss)
 
 
     def forward(self, X):
